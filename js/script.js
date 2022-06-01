4ec87235a12a2108c73e3ae94db0b5ef53414c38
RENZOZ78 <didierbap@gmail.com>
2022-06-01 15:53:32 +0200

    gallery - data filter bouton

--- js/script.js
+++ js/script.js
@@ -46,10 +46,25 @@ $(document).ready(function(){
 
     //Features - choisir l'image cliquée
     $('.small-image img').click(function(){
-        
+
         $(this).addClass('image-active').siblings().removeClass('image-active');
         let image = $(this).attr('src');
         $('.big-image img').attr('src', image);
+
+    });
+
+    //Gallery - 
+    $('.gallery .btn').click(function(){
+
+        let filter = $(this).attr('data-filter');
+        if(filter == 'all'){
+            $('.gallery .box').show(400);
+        }else{
+            $('.gallery .box').not('.'+filter).hide(200);
+            $('.gallery .box').filter('.'+filter).show(400);
+        }
+
+        $(this).addClass('button-active').siblings().removeClass('button-active');
 
     });
     
